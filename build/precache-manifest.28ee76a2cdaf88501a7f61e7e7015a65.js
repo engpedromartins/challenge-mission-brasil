ae010b9ab2c2c4d02dbaede5d688e14a21a87ab5
Pedro Martins <ispedromartins@gmail.com>
2020-10-08 02:27:42 -0300

    did the build

---
+++ build/precache-manifest.28ee76a2cdaf88501a7f61e7e7015a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9cc2be50e1342d64218bef72498214",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fffc87ab6f5ab36cbf75",
+    "url": "/static/css/main.f34cce5a.chunk.css"
+  },
+  {
+    "revision": "2abc926cb20f9b8af292",
+    "url": "/static/js/2.365707fe.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.365707fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fffc87ab6f5ab36cbf75",
+    "url": "/static/js/main.7afd76a4.chunk.js"
+  },
+  {
+    "revision": "18de4390a55bd4123f3e",
+    "url": "/static/js/runtime-main.938b69c2.js"
+  }
+]);
